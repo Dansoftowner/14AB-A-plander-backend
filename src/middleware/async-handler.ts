c24444f5f279bb8c857562823c6692b6890d7599
Daniel Gyoerffy <judalion777@gmail.com>
2023-11-18 14:20:00 +0100

    :art: Error handling logic for async route-handlers

---
+++ src/middleware/async-handler.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express'
+
+/**
+ * A wrapper function for async route handlers: passes the potentially occuring error
+ * to the next middleware (ideally the error-handling middleware)
+ */
+export default (handler: (req: Request, res: Response) => Promise<any>) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await handler(req, res)
+    } catch (e) {
+      next(e)
+    }
+  }
+}
