275939682bad1de06c985a6b838806207b09e7aa
Györffy.Dániel <judalion777@gmail.com>
2023-11-21 12:39:59 +0100

    :construction: Refactor associations api handling

--- src/api/association/association-controller.ts
+++ src/api/association/association-controller.ts
@@ -3,6 +3,11 @@ import { Controller } from '../../base/controller'
 import AssociationService from './association-service'
 import associationModel from './association-model'
 
+const DEFAULT_OFFSET = 0
+const DEFAULT_LIMIT = 10
+const DEFAULT_PROJECTION = 'lite'
+const DEFAULT_ORDER_BY = 'name'
+
 export default class AssociationController implements Controller {
   private associationService: AssociationService
 
@@ -13,21 +18,8 @@ export default class AssociationController implements Controller {
   async getAssociations(req: Request, res: Response) {
     const { offset, limit, projection, orderBy, q } = this.getPaginationData(req)
 
-    const associations = await associationModel
-      .find(
-        q
-          ? {
-              name: {
-                $regex: new RegExp(`.*${q}.*`, 'i'),
-              },
-            }
-          : {},
-      )
-      .skip(offset)
-      .limit(limit)
-      .sort(orderBy?.toString() ?? 'name')
-      .select(projection === 'full' ? '-__v' : '_id name')
-
+    //const items = this.associationService.getAssociations()
+    DEFAULT_OFFSET
     res.json({
       metadata: {
         total: await associationModel.countDocuments(),
--- src/api/association/association-service.ts
+++ src/api/association/association-service.ts
@@ -1,15 +1,33 @@
+import { FilterQuery } from 'mongoose'
 import { Service } from '../../base/service'
 import { Association } from './association'
 import associationModel from './association-model'
 
 export default class AssociationService implements Service {
-  getAssociations(
-    offset: number = 0,
-    limit: number = 10,
-    projection: string = 'lite',
-    orderBy: string = 'name',
-    q: string = '',
-  ): Association[] {
-    return associationModel.find({}) as unknown as Association[]
+  async getAssociations({
+    offset,
+    limit,
+    projection,
+    sort,
+    searchTerm,
+  }): Promise<Association[]> {
+    const associations = await associationModel
+      .find(this.getFilter(searchTerm))
+      .skip(offset)
+      .limit(limit)
+      .sort(sort)
+      .select(projection)
+
+    return associations
+  }
+
+  private getFilter(searchTerm: string): FilterQuery<Association> {
+    return searchTerm
+      ? {
+          name: {
+            $regex: new RegExp(`.*${searchTerm}.*`, 'i'),
+          },
+        }
+      : {}
   }
 }
