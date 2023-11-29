de63859fa1e9a51af32df4775b0ce1c850eb0cda
Daniel Gyoerffy <judalion777@gmail.com>
2023-11-29 20:28:27 +0100

    :globe_with_meridians: Internationalize joi validation messages

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -2,10 +2,14 @@ import { NextFunction, Request, Response } from 'express'
 import Joi from 'joi'
 import { ApiError } from '../api/error/api-error'
 import { ApiErrorCode } from '../api/error/api-error-codes'
+import { getResourceBundle } from '../utils/i18n'
 
 export default (validationSchema: Joi.Schema) =>
   (req: Request, res: Response, next: NextFunction) => {
-    const validationResult = validationSchema.validate(req.body)
+    const validationResult = validationSchema.validate(req.body, {
+      abortEarly: true,
+      messages: getResourceBundle(req.language, 'validation'),
+    })
 
     if (validationResult.error)
       throw new ApiError(
