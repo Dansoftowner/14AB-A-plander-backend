40a51728ba037caed854c4f9990735a1fbc8e97f
Daniel Gyoerffy <judalion777@gmail.com>
2023-12-15 20:23:32 +0100

    :memo: Update swagger docs

--- src/dto/forgotten-password.ts
+++ src/dto/forgotten-password.ts
@@ -1,6 +1,25 @@
 import Joi from 'joi'
 import { JoiObjectId, JoiPassword } from '../utils/joi'
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    ForgottenPassword:
+ *      type: object
+ *      properties:
+ *        association:
+ *          type: string
+ *          description: 'Unique identifier of the association'
+ *          example: '652f7b95fc13ae3ce86c7cdf'
+ *        email:
+ *          type: string
+ *          description: 'The email address of the member who wants to restore his password'
+ *          example: 'bverchambre0@alibaba.com'
+ *      required:
+ *        - association
+ *        - email
+ */
 export class ForgottenPasswordDto {
   association!: string
   email!: string
@@ -13,6 +32,21 @@ export class ForgottenPasswordDto {
   }
 }
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    NewPassword:
+ *      type: object
+ *      properties:
+ *       password:
+ *          type: string
+ *          description: 'The new password that will replace the forgotten password'
+ *          minLength: 8
+ *          example: 'Orange123'
+ *      required:
+ *        - password
+ */
 export class NewPasswordDto {
   password!: string
 
