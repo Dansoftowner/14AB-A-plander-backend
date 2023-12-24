9ee3626c77b716b3b3171a90bc05662e3ebae6a4
Daniel Gyoerffy <judalion777@gmail.com>
2023-12-24 21:45:20 +0100

    :christmas_tree: :memo: Add swagger docs for roles transfering endpoint

--- src/dto/roles-transfer-result.ts
+++ src/dto/roles-transfer-result.ts
@@ -1,5 +1,26 @@
 import { Expose } from 'class-transformer'
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *   RolesTransferResult:
+ *     type: object
+ *     properties:
+ *      _id:
+ *        type: string
+ *        description: 'Unique identifier of the member who recieved new roles.'
+ *        example: '652f866cfc13ae3ce86c7ce7'
+ *      roles:
+ *        type: array
+ *        description: 'Indicates what roles the member has (member, president)'
+ *        items:
+ *          type: string
+ *        minItems: 1
+ *        maxItems: 2
+ *        uniqueItems: true
+ *        example: ['member', 'president']
+ */
 export class MemberRolesTransferResultDto {
   @Expose()
   _id!: string
