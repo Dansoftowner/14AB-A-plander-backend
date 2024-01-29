dfa02b5b2d673ac315d038ab7d2bc986ec332bd2
Daniel Gyoerffy <judalion777@gmail.com>
2024-01-29 19:11:44 +0100

    :memo: Add swagger docs

--- src/api/routes/reports.ts
+++ src/api/routes/reports.ts
@@ -36,7 +36,7 @@ export class ReportRoutes extends RoutesProvider {
      *        schema:
      *          type: string
      *          required: true
-     *          description: The unique id of the assignment.
+     *        description: The unique id of the assignment.
      *    requestBody:
      *      required: true
      *      content:
@@ -73,6 +73,49 @@ export class ReportRoutes extends RoutesProvider {
       asyncErrorHandler((req, res) => controller.createReport(req, res)),
     )
 
+    /**
+     * @openapi
+     * /api/assignments/{id}/report/pdf:
+     *  get:
+     *    tags:
+     *      - Reports
+     *    description: |
+     *       Generates a PDF document for the given report.
+     *
+     *       **Authentication is required** before using this endpoint.
+     *    parameters:
+     *      - in: path
+     *        name: id
+     *        schema:
+     *          type: string
+     *          required: true
+     *        description: The unique id of the assignment.
+     *    responses:
+     *      200:
+     *        description: PDF is successfully generated.
+     *        content:
+     *          application/pdf:
+     *            schema:
+     *              type: string
+     *              format: binary
+     *      400:
+     *        $ref: '#/components/responses/InvalidObjectId'
+     *      401:
+     *        $ref: '#/components/responses/Unauthorized'
+     *      404:
+     *        description: |
+     *          Either the assignment or the report doesn't exist.
+     *
+     *          Error code might be `missing-resource` or `report-not-exist`.
+     *        content:
+     *          application/json:
+     *            schema:
+     *              $ref: '#/components/schemas/Error'
+     *      429:
+     *        $ref: '#/components/responses/SurpassedRateLimit'
+     *      5XX:
+     *        $ref: '#/components/responses/InternalServerError'
+     */
     this.router.get(
       '/:id/report/pdf',
       auth,
