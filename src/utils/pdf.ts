244fd882d2392d4bc62fdb535769150af6e1e0c7
Daniel Gyoerffy <judalion777@gmail.com>
2024-01-30 18:56:14 +0100

    :ambulance: Chromium for pdf doesn't launch in the cloud

--- src/utils/pdf.ts
+++ src/utils/pdf.ts
@@ -2,7 +2,7 @@ import puppeteer, { PDFOptions } from 'puppeteer'
 import { Readable } from 'stream'
 
 export async function convertHtmlToPdf(html: string, options: PDFOptions | undefined): Promise<Readable> {
-  const browser = await puppeteer.launch({ headless: 'new' })
+  const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] })
   const page = await browser.newPage()
 
   await page.setContent(html, { waitUntil: 'domcontentloaded' })
