00cbb72f16665d4fb68ad0026c7eea035107dc31
Györffy.Dániel <judalion777@gmail.com>
2024-03-06 08:25:42 +0100

    :safety_vest: Allow dashes in member full names

--- src/utils/common-regex.ts
+++ src/utils/common-regex.ts
@@ -1,4 +1,4 @@
-export const SPECIAL_CHARACTERS = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
+export const SPECIAL_CHARACTERS = /[!@#$%^&*()_+=\[\]{};':"\\|,.<>\/?~]/
 export const guardNumberPattern = () => /\d{2}\/\d{4}\/\d{5}/g
 export const fullNamePattern = () => /^[^\d]+\s[^\d]+(\s[^\d]+)*$/g
 export const emailPattern = () => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
